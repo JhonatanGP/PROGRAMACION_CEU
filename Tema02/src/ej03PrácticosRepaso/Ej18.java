797a2a67a8fee9b721a03cc215c776d33e361374
JhonatanGP <jguzman5292@ceuandalucia.es>
2024-10-23 20:50:21 +0200

    s

---
+++ Tema02/src/ej03PrácticosRepaso/Ej18.java
@@ -0,0 +1,20 @@
+package ej03PrácticosRepaso;
+
+import java.util.Scanner;
+
+public class Ej18 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Escriba una palabra y vea como se ve al revés:");
+		String p = sc.nextLine();
+				
+		for(int i = p.length()-1; i>=0 ; i--) {
+			System.out.print(p.charAt(i)); // charAt: devuelve la letra que esta en esa posición
+		}
+					
+		sc.close();
+	}
+}
