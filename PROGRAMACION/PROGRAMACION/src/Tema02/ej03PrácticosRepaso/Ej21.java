0166462de9055b58270d8d37816513bb60e373f4
JhonatanGP <jguzman5292@ceuandalucia.es>
2024-10-25 19:02:24 +0200

    s

---
+++ PROGRAMACION/PROGRAMACION/src/Tema02/ej03PrácticosRepaso/Ej21.java
@@ -0,0 +1,15 @@
+package Tema02.ej03PrácticosRepaso;
+
+import java.util.Scanner;
+
+public class Ej21 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		Boolean v = true;
+		
+
+		sc.close();
+	}
+}
