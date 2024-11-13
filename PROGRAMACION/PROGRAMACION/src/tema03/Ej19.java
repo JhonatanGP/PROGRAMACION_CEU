5d310f63f66bd55b1a7d2a8a9c165e4f997d13ad
JhonatanGP <jguzman5292@ceuandalucia.es>
2024-11-13 20:50:38 +0100

    e

---
+++ PROGRAMACION/PROGRAMACION/src/tema03/Ej19.java
@@ -0,0 +1,17 @@
+package tema03;
+
+import java.util.Random;
+
+public class Ej19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] lista = new String[] {"azulejo", "tenedor", "saltamontes", "carretilla",
+			"molinero", "sofisticado", "terremoto", "culinario", "teclado", "primavera" };
+		
+		Random r = new Random();
+		Integer aleatorio = r.nextInt(lista.length);
+		
+	}
+
+}
