5134960b157fb08156511af26fc81e92504d9829
JhonatanGP <jguzman5292@ceuandalucia.es>
2024-11-18 17:51:58 +0100

    j

--- PROGRAMACION/PROGRAMACION/src/tema03/ejRepaso/Ej02.java
+++ PROGRAMACION/PROGRAMACION/src/tema03/ejRepaso/Ej02.java
@@ -5,7 +5,7 @@ public class Ej02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Integer[] lista = new Integer[] { 7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10 };
-		Integer suma = 0;
+		Double suma = 0.0;
 		Integer cont = 0;
 		for (int i = 0; i < lista.length; i++) {
 			if (lista[i] >= 5) {
--- PROGRAMACION/PROGRAMACION/src/tema03/ejRepaso/Ej03.java
+++ PROGRAMACION/PROGRAMACION/src/tema03/ejRepaso/Ej03.java
@@ -8,6 +8,19 @@ public class Ej03 {
 		Integer[] lista = new Integer[] { 7, 9, 8, 4, 0, 6, 3, 4, 1, 7, 3, 1, 9, 10 };
 		Scanner sc = new Scanner(System.in);
 		System.out.println("¿Qué notas buscas?");
-		Integer num = sc.nextInt();
+		Integer n = sc.nextInt();
+		Boolean encontrada = false; // Variable para controlar si se encuentra la nota
+
+		for (int i = 0; i < lista.length; i++) {
+			if (lista[i] == n) {
+				System.out.println("La nota buscada existe y está en la posición: " + i + ".");
+				encontrada = true;
+				break;
+			}
+		}
+		if (!encontrada) {
+			System.out.println("La nota buscada no existe.");
+		}
+		sc.close();
 	}
 }
