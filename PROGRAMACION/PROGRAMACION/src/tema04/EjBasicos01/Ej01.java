4992ab0e9378a981950ee3588b2445d2674df4b0
JhonatanGP <jguzman5292@ceuandalucia.es>
2025-01-08 20:43:46 +0100

    yh

--- PROGRAMACION/PROGRAMACION/src/tema04/EjBasicos01/Ej01.java
+++ PROGRAMACION/PROGRAMACION/src/tema04/EjBasicos01/Ej01.java
@@ -3,7 +3,7 @@ package tema04.EjBasicos01;
 public class Ej01 {
 
 	public static void main(String[] args) {
-		String cad1 = " Frase aleatoria con espacios. ";
+		String cad1 = "     Frase aleatoria con espacios.         ";
 		String resultado = getMayuscula(cad1);
 		System.out.println(resultado);
 	}
