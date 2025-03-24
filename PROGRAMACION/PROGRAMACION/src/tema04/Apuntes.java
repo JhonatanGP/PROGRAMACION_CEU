1bf71ddffff586d1d8701ed044e44f14afa957f7
JhonatanGP <jguzman5292@ceuandalucia.es>
2025-03-24 17:44:42 +0100

    d

--- PROGRAMACION/PROGRAMACION/src/tema04/Apuntes.java
+++ PROGRAMACION/PROGRAMACION/src/tema04/Apuntes.java
@@ -53,11 +53,61 @@ public class Apuntes {
 		BigDecimal se compara con compareTo
 		add es el que suma en BigDecimal
 		
-		-- MAPAS
-		Para recorrer un map, encontrar KEY: this.mapa.containsKey(key);
-		
-		
-		
+***************************** TERCER TRIMESTRE *************************************************************** 
+-- MAPAS
+	
+	Para recorrer un map, encontrar KEY: this.mapa.containsKey(key);
+				
+	Mapa guarda de dos en dos (clave y valor)
+	Clave:Es única.(Es lo más importante).
+	Valor: Si puede tener dos valores iguales.
+	Si usa una clave que existe la sobre escribe (No pueden haber dos claves iguales).
+	No hay orden.
+ 
+	Map <String,Integer> mapa1 = new HashMap<>();
+  
+	mapa1.put("CLAVE",1234567);
+ 
+	mapa1.get("CLAVE");
+ 
+	mapa1.remove("CLAVE");
+ 
+	Metodos:
+	isEmpty();
+	clear();
+	containsKey(); Si la clave esta --> Devuelve Boolean (Mira el equals)
+	containsValue(); Si el valor esta --> Devuelve Boolean (Mira el equals)
+ 
+	Borrar de forma alternativa: mapa.put("Clave", null);
+	Como esta nulo seguira existiendo pero no estará asociada a ningun valor.
+  
+  
+	RECORRER MAPAS:
+ 
+	1) Set<String> claves = mapa.keySet();
+		for(String key: pares){
+		clave = key
+		Valor = mapa.get(key);
+		}
+ 
+ 
+	Collection<Alumno> values = mapa.values(); (Recorrer para traer los valores, si te pide clave no se puede usar)
+   	for(Alumno alumno: values){
+		syso(alumno);
+	}
+ 
+  	Collection<Alumno> values = mapa.values();
+  	List<Alumno> lista = new ArrayList<>(values);
+   	for(Alumno alumno: lista){
+		syso(alumno);
+	}
+ 
+ 
+	Set<Entry<String,Alumno>> pares = mpa.entrySet(); 
+   	for(Entry<String,Alumno>> par : pares){
+		syso("Clave:" + par.getKey());
+		syso("Valor:" + par.getValue());
+	}
 		
 		
 		
