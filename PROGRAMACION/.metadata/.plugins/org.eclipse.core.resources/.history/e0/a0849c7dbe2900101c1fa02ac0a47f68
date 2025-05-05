dac4eceb27bf7f825717608863fff345a9a574b4
JhonatanGP <jguzman5292@ceuandalucia.es>
2025-05-05 17:51:05 +0200

    j

---
+++ PROGRAMACION/.metadata/.plugins/org.eclipse.core.resources/.history/e0/a0849c7dbe2900101c1fa02ac0a47f68
@@ -0,0 +1,10 @@
+package tema06.EjBasicos.Ej07.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ PROGRAMACION/PROGRAMACION/src/tema06/EjBasicos/Ej09/app/Test.java
@@ -0,0 +1,10 @@
+package tema06.EjBasicos.Ej09.app;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
